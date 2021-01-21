642b37d5acfd3b7865d3f9da1d202838e5449060
Omar Polo <op@omarpolo.com>
2021-01-21 14:48:26 +0000

    don't include config.h there

--- compat/strlcpy.c
+++ compat/strlcpy.c
@@ -13,7 +13,6 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-#include "config.h"
 
 #include <sys/types.h>
 #include <string.h>
