5d160453f27672bda82e8497aa96a4230705ffbd
Omar Polo <op@omarpolo.com>
2023-06-11 12:45:42 +0000

    remove proc_ispeer()
    unused, and was dropped by other copies of proc.c; reduces the diff
    with httpd' proc.c.

--- proc.c
+++ proc.c
@@ -49,23 +49,10 @@ void	 proc_open(struct privsep *, int, int);
 void	 proc_accept(struct privsep *, int, enum privsep_procid,
 	    unsigned int);
 void	 proc_close(struct privsep *);
-int	 proc_ispeer(struct privsep_proc *, unsigned int, enum privsep_procid);
 void	 proc_shutdown(struct privsep_proc *);
 void	 proc_sig_handler(int, short, void *);
 void	 proc_range(struct privsep *, enum privsep_procid, int *, int *);
 int	 proc_dispatch_null(int, struct privsep_proc *, struct imsg *);
-
-int
-proc_ispeer(struct privsep_proc *procs, unsigned int nproc,
-    enum privsep_procid type)
-{
-	unsigned int	i;
-
-	for (i = 0; i < nproc; i++)
-		if (procs[i].p_id == type)
-			return (1);
-	return (0);
-}
 
 enum privsep_procid
 proc_getid(struct privsep_proc *procs, unsigned int nproc,
