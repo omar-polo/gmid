60f4107da6ed88a34867cdcbf63794b5dc039f94
Omar Polo <op@omarpolo.com>
2023-07-23 19:04:53 +0000

    add a test with fastcgi, locations and forceful disabling

--- regress/tests.sh
+++ regress/tests.sh
@@ -238,6 +238,32 @@ test_fastcgi() {
 	return 0
 }
 
+test_fastcgi_inside_location() {
+	./fcgi-test fcgi.sock &
+	fcgi_pid=$!
+
+	setup_simple_test 'prefork 1' 'fastcgi socket "'$PWD'/fcgi.sock"
+	location "/dir/*" {
+		fastcgi off
+	}'
+
+	msg=$(printf "# hello from fastcgi!\nsome more content in the page...")
+	fetch /foo
+	if ! check_reply "20 text/gemini" "$msg"; then
+		kill $fcgi_pid
+		return 1
+	fi
+
+	fetch /dir/foo.gmi
+	if ! check_reply "20 text/gemini" "# hello world"; then
+		kill $fcgi_pid
+		return 1
+	fi
+
+	kill $fcgi_pid
+	return 0
+}
+
 test_fastcgi_deprecated_syntax() {
 	./fcgi-test fcgi.sock &
 	fcgi_pid=$!
