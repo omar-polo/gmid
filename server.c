ced97516f9f02bd1a970d18038ce16b2ebcfcafe
Christoph Liebender <christoph.liebender@posteo.de>
2024-06-17 18:44:33 +0200

    don't close socket too early, add warning message

--- server.c
+++ server.c
@@ -1343,8 +1343,8 @@ read_cb(struct tls *ctx, void *buf, size_t buflen, void *cb_arg)
 	
 	int parse_status = proxy_proto_v1_parse(&pp1, c->buf.data, c->buf.len, &consumed);
 	if (PROXY_PROTO_PARSE_SUCCESS != parse_status) {
-		close(c->fd);
-		return 0;
+		log_warnx("read_cb: received invalid proxy protocol header");
+		return -1;
 	}
 
 	switch (pp1.proto) {
